3231978ee2626c56a17064bc0920858265b9ba8c
Kateryna Balashova <katybalashov@gmail.com>
2025-11-23 15:55:13 +0200

    create authentication and authorization

---
+++ src/models/user.js
@@ -0,0 +1,39 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+userSchema.methods.toJSON = function () {
+  const odj = this.toObject();
+  delete odj.password;
+  return odj;
+};
+
+userSchema.pre('save', function (next) {
+  if (!this.username) {
+    this.username = this.email;
+  }
+  next();
+});
+
+export const User = model('User', userSchema);
